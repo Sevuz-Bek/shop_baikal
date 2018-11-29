e290ad2127fa4548797e0ee8e6ff65bc08a8b6c9
Sevuz-Bek <gaidukov83@mail.ru>
2018-11-29 16:38:48 +0300

    slider_price

--- js/jq.js
+++ js/jq.js
@@ -1,1 +1,27 @@
+$(function(){
 
+    "use strict";
+    $('.title_box').click(function(){
+        
+        $('this').toggleClass('open');
+        $(this).next('.list_link').toggleClass('open');
+    });
+
+    $('#slider_price').slider({
+        max: 1000,
+        min: 0,
+        range: true,
+        values: [ 190, 728 ],
+        slider: function( event, ui ) {
+            $('input[name="minPrice"]').val( '$' + ui.values[0] );
+            $('input[name="maxPrice"]').val( '$' + ui.values[1] );
+        }
+    });
+
+    $('input[name="minPrice"]').val( '$' + 
+    $('#slider_price').slider('values',0) );
+    $('input[name="maxPrice"]').val( '$' + 
+    $('#slider_price').slider('values',1));
+
+
+});
